6a0ace728200cafab453245d6dbe4223f7d46338
gitiboys <82661142+gitiboys@users.noreply.github.com>
2021-05-05 09:51:53 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,10 +4,4 @@ from pyrogram import Client, Filters
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
     helptxt = f"<i>Send any Youtube link...</i>"
-    await message.reply_text(helptxt)
-    
-    
-@Client.on_message(Filters.command(["contact"]))
-async def start(client, message):
-    contxt = f"<code>Send your feedback through</code> @de5tr0yer_bot 👈"
-    await message.reply_text(contxt)    
+    await message.reply_text(helptxt)   
