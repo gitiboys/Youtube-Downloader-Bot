ccb09319463fb15f5b85d0590536eeed37eb40e5
gitiboys <82661142+gitiboys@users.noreply.github.com>
2021-05-05 09:52:28 +0530

    Create contact.py

---
+++ plugins/contact.py
@@ -0,0 +1,7 @@
+
+
+
+@Client.on_message(Filters.command(["contact"]))
+async def start(client, message):
+    contxt = f"<code>Send your feedback through</code> @de5tr0yer_bot 👈"
+    await message.reply_text(contxt) 
