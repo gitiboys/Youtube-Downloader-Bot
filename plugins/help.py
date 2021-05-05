7f540ef8420e9aacb9ea4dd34de20ef1e60c76ca
gitiboys <82661142+gitiboys@users.noreply.github.com>
2021-05-05 09:29:27 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -7,5 +7,5 @@ async def start(client, message):
     await message.reply_text(helptxt)
 @Client.on_message(Filters.command(["contact"]))
 async def start(client, message):
-    contxt = f"<code>Send your feedback through</code> @de5tr0yer_bot"
+    contxt = f"<code>Send your feedback through</code> @de5tr0yer_bot 👈"
     await message.reply_text(contxt)    
