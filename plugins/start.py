7cfc2f09658e23bed1b2998e74b672129b5743c8
gitiboys <82661142+gitiboys@users.noreply.github.com>
2021-05-05 09:20:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/TeluguBots")]
+        [InlineKeyboardButton("❤️ SHARE IT ❤️", url="https://t.me/share/url?url=Check%20this%20🤩%20amazing%20bot%20✅:%20https://t.me/MohithaVarma999_Bot")]
     ])
     welcomed = f"Hey {message.from_user.first_name}\n\nThis is Youtube Downloader 📽\n\nHit 👉 /help"
     await message.reply_text(welcomed, reply_markup=joinButton)
